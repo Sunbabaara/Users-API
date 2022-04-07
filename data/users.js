9c845b7f3610765ef7e06c561489dd13095f6ba0
Sunbabaara <csunbabaara@gmail.com>
2022-04-07 15:45:18 +0000

    first commit

---
+++ data/users.js
@@ -0,0 +1,8 @@
+const users=[
+    {id:1, name:"John", age:25},
+    {id:2, name:"Pete", age:27},
+    {id:3, name:"Akosua", age:22},
+    {id:4, name:"Andy", age:20},
+    {id:5, name:"Mary", age:28}
+]
+module.exports=users
