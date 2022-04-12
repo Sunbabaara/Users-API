574ab4a8c1b610981cfaaf285291afbed8e443e9
Sunbabaara <csunbabaara@gmail.com>
2022-04-12 15:01:05 +0000

    added middleware

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const express =require("express")
 const userRoute = require("./routes/usersRoutes")
+const path = require("path")
+const logger = require("./middlewares/user.logger")
+const booksRoute = require("./routes/booksRoute")
 
 const app = express()
 
 //middlewares
 app.use(express.json());
-
+app.use(logger)
 app.use(userRoute)
+app.use(booksRoute)
 
 //home route
 app.get("/", (req, res)=>{
-res.status(200).send("<h1>WELCOME TO THE USERS DATABASE</h1>");
+res.sendFile(path.join(__dirname + "/pages/index.html"))
+})
+
+app.get("/*", (req, res)=>{
+    res.status(400).sendFile(path.join(__dirname + "/pages/404.html"))
 })
 
 const PORT = 4000;
